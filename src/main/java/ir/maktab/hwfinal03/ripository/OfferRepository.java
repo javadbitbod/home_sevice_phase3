8e0f7940284d0f138284f6b5d125484d0ec246ee
javad <javadbitbod@gmail.com>
2023-11-01 20:52:36 +0330

    rename class

--- src/main/java/ir/maktab/hwfinal03/ripository/OfferRepository.java
+++ src/main/java/ir/maktab/hwfinal03/ripository/OfferRepository.java
@@ -1,7 +1,7 @@
 package ir.maktab.hwfinal03.ripository;
 
-import ir.maktab.hwfinal03.entity.Offers;
+import ir.maktab.hwfinal03.entity.Offer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OfferRepository extends JpaRepository<Offers,Long> {
+public interface OfferRepository extends JpaRepository<Offer,Long> {
 }
