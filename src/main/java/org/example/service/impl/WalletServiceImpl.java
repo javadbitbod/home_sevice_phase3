4ee51fe7ee2bcb7cffa2e70d13119b0f36f03fc4
javad <javadbitbod@gmail.com>
2023-11-02 22:10:26 +0330

    create WalletServiceImpl by methods

---
+++ src/main/java/org/example/service/impl/WalletServiceImpl.java
@@ -0,0 +1,52 @@
+package org.example.service.impl;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.example.dto.WalletDTO;
+import org.example.entity.Wallet;
+import org.example.exception.NotFoundTheUserException;
+import org.example.mapper.WalletMapper;
+import org.example.repository.WalletRepository;
+import org.example.service.WalletService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class WalletServiceImpl implements WalletService {
+
+    private final WalletRepository walletRepository;
+    private final WalletMapper walletMapper = new WalletMapper();
+
+    @Override
+    public Optional<WalletDTO> findAdminWalletByEmailAndPassword(String email, String password) {
+        Optional<Wallet> wallet = walletRepository.findAdminWalletByEmailAndPassword(email, password);
+        if (wallet.isEmpty()){
+            throw new NotFoundTheUserException("not found the user.");
+        } else {
+            return Optional.ofNullable(walletMapper.convert(wallet.get()));
+        }
+    }
+
+    @Override
+    public Optional<WalletDTO> findClientWalletByEmailAndPassword(String email, String password) {
+        Optional<Wallet> wallet = walletRepository.findClientWalletByEmailAndPassword(email, password);
+        if (wallet.isEmpty()){
+            throw new NotFoundTheUserException("not found the user.");
+        }else {
+         return Optional.ofNullable(walletMapper.convert(wallet.get()));
+        }
+    }
+
+    @Override
+    public Optional<WalletDTO> findExpertWalletByEmailAndPassword(String email, String password) {
+        Optional<Wallet> wallet = walletRepository.findExpertWalletByEmailAndPassword(email, password);
+        if (wallet.isEmpty()){
+            throw new NotFoundTheUserException("not found the user.");
+        }else {
+            return Optional.ofNullable(walletMapper.convert(wallet.get()));
+        }
+    }
+}
