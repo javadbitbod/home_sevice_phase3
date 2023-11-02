0eac535387298543f59e8a8f7fcba18a5a0713a5
javad <javadbitbod@gmail.com>
2023-11-02 11:08:05 +0330

    add findSubServicesByServiceName&Description&Service methods in SubServiceRepository

--- src/main/java/ir/maktab/hwfinal03/ripository/SubServiceRepository.java
+++ src/main/java/ir/maktab/hwfinal03/ripository/SubServiceRepository.java
@@ -3,11 +3,25 @@ package ir.maktab.hwfinal03.ripository;
 import ir.maktab.hwfinal03.entity.Services;
 import ir.maktab.hwfinal03.entity.SubService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface SubServiceRepository extends JpaRepository<SubService,Long> {
     Optional<SubService> findByDescriptionAndService(String description, Services service);
+    @Query("select s from SubService s join s.expertList e where e.id = :id")
+    List<SubService> findByExpertId(Long id);
+
+    @Query("select s from SubService s where s.service.id = :id")
+    List<SubService> findSubServicesByServiceId(Long id);
+
+    List<SubService> findSubServicesByService(Services service);
+
+    Optional<SubService> findSubServiceByDescription(String description);
+
+    List<SubService> findSubServicesByServiceName(String name);
+
 
 }
