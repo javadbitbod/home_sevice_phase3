4dd773d19aa6c7d7c6a171fb8293ce7ffb5f289d
javad <javadbitbod@gmail.com>
2023-11-02 11:02:54 +0330

    add findServiceByName method in ServicesRepository

--- src/main/java/ir/maktab/hwfinal03/ripository/ServicesRepository.java
+++ src/main/java/ir/maktab/hwfinal03/ripository/ServicesRepository.java
@@ -4,7 +4,10 @@ package ir.maktab.hwfinal03.ripository;
 import ir.maktab.hwfinal03.entity.Services;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ServicesRepository extends JpaRepository<Services,Long> {
+    Optional<Services> findServiceByName(String name);
 
 }
 
