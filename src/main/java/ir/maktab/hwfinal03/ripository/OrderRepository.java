4c0e7bf0f2423bf4d5bbaf5bc51a879296bc44fe
javad <javadbitbod@gmail.com>
2023-11-02 14:56:09 +0330

    Add method need

--- src/main/java/ir/maktab/hwfinal03/ripository/OrderRepository.java
+++ src/main/java/ir/maktab/hwfinal03/ripository/OrderRepository.java
@@ -1,7 +1,36 @@
 package ir.maktab.hwfinal03.ripository;
 
 import ir.maktab.hwfinal03.entity.Order;
+import ir.maktab.hwfinal03.entity.SubService;
+import ir.maktab.hwfinal03.entity.enumeration.OrderStatus;
+import ir.maktab.hwfinal03.entity.users.Client;
+import ir.maktab.hwfinal03.entity.users.Expert;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
+    @Query("select o from Order o where o.client.id = :id")
+    List<Order> findOrdersByClientId(Long id);
+
+    @Query("select o from Order o where o.expert.id = :id")
+    List<Order> findOrdersByExpertId(Long id);
+
+    @Query("select o from Order o where o.subService.id = :id")
+    List<Order> findOrdersBySubServiceId(Long id);
+
+    List<Order> findOrdersByClient(Client client);
+
+    List<Order> findOrdersByExpert(Expert expert);
+
+    List<Order> findOrdersBySubService(SubService subService);
+
+    List<Order> findOrdersByOrderStatus(OrderStatus orderStatus);
+
+    @Query("select count(o.client.id) from Order o where o.client.id= :id")
+    int countOrdersByClientId(Long id);
+
+    @Query("select count(o.client.email) from Order o where o.client.email= :email")
+    Integer countOrdersByClientEmail(String email);
 }
