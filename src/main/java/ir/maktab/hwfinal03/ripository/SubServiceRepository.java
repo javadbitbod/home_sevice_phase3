1a7414ff01c53a3d4f053603d51a57a4b9a613a9
javad <javadbitbod@gmail.com>
2023-11-02 11:04:55 +0330

    add findByDescriptionAndService method in SubServiceRepository

--- src/main/java/ir/maktab/hwfinal03/ripository/SubServiceRepository.java
+++ src/main/java/ir/maktab/hwfinal03/ripository/SubServiceRepository.java
@@ -1,8 +1,13 @@
 package ir.maktab.hwfinal03.ripository;
 
+import ir.maktab.hwfinal03.entity.Services;
 import ir.maktab.hwfinal03.entity.SubService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface SubServiceRepository extends JpaRepository<SubService,Long> {
+    Optional<SubService> findByDescriptionAndService(String description, Services service);
+
 }
