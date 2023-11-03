1c19bd2571819694a293ea2c2a5edc568a1b1b0c
javad <javadbitbod@gmail.com>
2023-11-03 04:20:56 +0330

    create confirmExpert & addExpertToSubService method to AdminController

--- src/main/java/org/example/controller/AdminController.java
+++ src/main/java/org/example/controller/AdminController.java
@@ -5,6 +5,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.dto.ServiceDTO;
 import org.example.dto.SubServiceDTO;
+import org.example.entity.users.enums.UserStatus;
 import org.example.service.AdminService;
 import org.example.service.ClientService;
 import org.example.service.ExpertService;
@@ -31,5 +32,13 @@ public class AdminController {
     @PutMapping("/edit-sub-service/{id}/{newBasePrice}/{newDescription}")
     public void editSubService(@PathVariable Long id, @PathVariable double newBasePrice, @PathVariable String newDescription) {
         adminService.editSubService(id, newBasePrice, newDescription);
+    }
+    @PutMapping("/confirm-expert/{expertId}")
+    public void confirmExpert(@PathVariable Long expertId){
+        adminService.editExpertStatus(expertId, UserStatus.CONFIRMED);
+    }
+    @PostMapping("/add-expert-to-sub-service/{expertId}/{subServiceId}")
+    public void addExpertToSubService(@PathVariable Long expertId, @PathVariable Long subServiceId) {
+        adminService.addExpertToSubService(expertId, subServiceId);
     }
 }
