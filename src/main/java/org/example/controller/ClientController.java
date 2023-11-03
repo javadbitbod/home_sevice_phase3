facfe4eb06c652b03adaa3ada960b28fbd452281
javad <javadbitbod@gmail.com>
2023-11-03 05:06:14 +0330

    create createOrder method to ClientController

--- src/main/java/org/example/controller/ClientController.java
+++ src/main/java/org/example/controller/ClientController.java
@@ -2,6 +2,7 @@ package org.example.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.dto.ClientDTO;
+import org.example.dto.OrderDTO;
 import org.example.service.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,5 +27,10 @@ public class ClientController {
     @PutMapping("/edit-client-password/{clientId}/{newPassword}")
     public void editClientPassword(@PathVariable Long clientId,@PathVariable String newPassword) {
         clientService.editClientPassword(clientId, newPassword);
+    }
+
+    @PostMapping("/create-order")
+    public void createOrder(@RequestBody OrderDTO orderDTO) {
+        clientService.createOrder(orderDTO);
     }
 }
